275a11791783cf21636c6a6265523d1cb4e641d3
Roland Exler <roland@exler-linz.at>
2024-12-07 21:50:33 +0100

    cosmetic changes

--- src/espDAC.cpp
+++ src/espDAC.cpp
@@ -1,10 +1,13 @@
 #include "webIO.h"
 
+// arduino like interface does not distinguish ADC1 and ADC2
+espADC webADC;
+
 String espDAC::help()
 {
     static const String helptext = 
         "Help on DAC subsystem\r\n"
-        "\nconfiguration for scaled output using raw==round(value*scale+offset)\r\n"
+        "\nconfiguration for scaled output using raw=round(value*scale+offset)\r\n"
         "  scale=float ... set scaling (default 1.0)\r\n"
         "  offset=float ... set offset (default 0.0)\r\n"
         "  disable ... disable DAC function on this channel (pin)\r\n"
@@ -14,17 +17,16 @@ String espDAC::help()
     return helptext;
 }
 
-void espDAC::raw(uint8_t value)
+void espDAC::setRaw(uint8_t value)
 {
     dacWrite(dacPin, value);
 }
 
 String espDAC::parse(String command, String value)
 {
-    // _serial.println(command + ":" + value);
     String result = "";
     if (command == "raw")
-        raw(value.toInt());
+        setRaw(value.toInt());
     else if (command == "offset")
         setOffset(value.toInt());
     else if (command == "scale")
