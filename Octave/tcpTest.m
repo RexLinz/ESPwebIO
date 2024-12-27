ff02888179c8116c93c05bf14ca6031c3c89a79f
Roland Exler <roland@exler-linz.at>
2024-12-27 18:44:52 +0100

    replace tcpQuery with writeread from instrument-control package

--- Octave/tcpTest.m
+++ Octave/tcpTest.m
@@ -7,33 +7,39 @@ pkg load instrument-control
 % ESP32 WebIO
 % host = resolvehost("WebIO");
 % port = 1026;
-webIO = tcpclient("WebIO", 1026, "Timeout", 1);
+webIO = tcpclient("WebIO", 1026, "Timeout", 2);
 if webIO.Status ~= "open"
   error("could not connect to device via TCP");
 end
+configureTerminator(webIO, 4); % 004 = End Of Text (EOT)
+flush(webIO);
 
-switch 3
+switch 6
   case 1 % get status and root help
-    disp(tcpQuery(webIO, "/status"));
-    disp(tcpQuery(webIO, "/"));
+    disp(writeread(webIO, "/status"));
+    disp(writeread(webIO, "/"));
   case 2 % query GPIO help
-    disp(tcpQuery(webIO, "/GPIO"));
+    disp(writeread(webIO, "/GPIO"));
   case 3 % query GPIO pin values
-    disp(tcpQuery(webIO, "/GPIO?state=21,22,23"))
+    disp(writeread(webIO, "/GPIO?state=21,22,23"))
   case 4 % query GPIO pin values
     disp("blinking LED at GPIO outputs 21,23");
-    tcpQuery(webIO, "/GPIO?output=21,22,23&set=21,22");
+    writeread(webIO, "/GPIO?output=21,22,23&set=21,22");
     disp("press any key to stop");
     do
-      disp(tcpQuery(webIO, "/GPIO?toggle=21,23"));
+      disp(writeread(webIO, "/GPIO?toggle=21,23"));
       pause(1.0);
     until length(kbhit(1)) > 0 % end on any keystroke
     disp("  stopped");
   case 5
+    disp("set DAC output values");
+    disp(writeread(webIO, "/DAC1?raw=50"));
+    disp(writeread(webIO, "/DAC2?raw=100"));
+  case 6
     disp("reading raw values from ADC input 34,35");
     disp("press any key to stop");
     do
-      disp(tcpQuery(webIO, "/ADC?raw=34,35"));
+      disp(writeread(webIO, "/ADC?raw=34,35"));
       pause(1.0);
     until length(kbhit(1)) > 0 % end on any keystroke
     disp("  stopped");
